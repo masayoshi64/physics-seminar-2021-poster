f8bebc711667e03c32782cd3d586125ad75b5f0d
masayoshi64 <sho32001025@gmail.com>
2021-09-11 14:30:32 +0900

    iroiro

--- plot.py
+++ plot.py
@@ -11,10 +11,15 @@ def main():
             with open("data/" + name + ".json") as f:
                 df = json.load(f)
                 title = df["title"]
-            df = pd.read_csv("data/" + name + "_L1.csv")
+
+            # df = pd.read_csv("data/" + name + "_L1.csv")
+            # df = pd.read_csv("data/" + name + "_MI.csv")
+            df = pd.read_csv("data/" + name + "_CI.csv")
+
             plt.errorbar(
                 df["rad_num"], df["ave"], yerr=df["std"], capsize=5, label=title
             )
+
     plt.legend()
     plt.show()
     return 0
